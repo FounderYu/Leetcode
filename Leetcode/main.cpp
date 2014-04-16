0545dabcd3e0ca03c3f83ff11eb63139c303874a
FounderYu <fangzhey@uci.edu>
2014-04-16 10:26:08 -0700

    update

---
+++ Leetcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by yu fangzheng on 4/16/14.
+//  Copyright (c) 2014 yu fangzheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
